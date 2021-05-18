2ca59dd10604355210981d0f218dcb37f3488434
reemalqurm <reem.alqurm@gmail.com>
2021-05-18 19:43:36 +0300

    all requerments is done

--- __test__/v2.test.js
+++ __test__/v2.test.js
@@ -1,6 +1,7 @@
 'use strict';
 process.env.SECRET = 'teo';
 const server = require('../src/server').server;
+
 const supergoose = require('@code-fellows/supergoose');
 const request = supergoose(server);
 let id;
@@ -24,47 +25,71 @@ describe('Testing server', () => {
         calories: "20",
         type: "PROTIEN"
     })
-    // const response = await request.post('/api/v2/food/').send({
-    //     name: "fish",
-    //     calories: "20",
-    //     type: "PROTIEN"
-    // })
+ 
     expect(response.status).toEqual(201);
     expect(response.body.type).toEqual("PROTIEN");
     id = response.body._id
   });
   // Update a record 
   it('Update a Food record', async () => {
-    const response = await request.put(`/api/v2/food/${id}`).send({
+    let obj = { username: 'admin', password: 'password', role: 'admin' };
+    const response2 = await request.post('/signup').send(obj);
+    const response1 = await request.post('/signin')
+    .auth(obj.username, obj.password);
+    const userObject = response1.body;
+    const response = await request.put(`/api/v2/food/${id}`).set('Authorization', `Bearer ${response1.body.token}`).send({
         name: "Salmofish",
         calories: "20",
         type: "PROTIEN"
     });
+   
     expect(response.status).toEqual(200);
     expect(response.body.type).toEqual('PROTIEN');
   });
   // Read a record
   it('Read a Food record', async () => {
-    const response = await request.get(`/api/v2/food/${id}`);
+    let obj = { username: 'admin', password: 'password', role: 'admin' };
+    const response2 = await request.post('/signup').send(obj);
+    const response1 = await request.post('/signin')
+    .auth(obj.username, obj.password);
+    const userObject = response1.body;
+    const response = await request.get(`/api/v2/food/${id}`).set('Authorization', `Bearer ${response1.body.token}`);
+
     expect(response.status).toEqual(200);
     expect(response.body.name).toEqual('Salmofish');
     expect(response.body.type).toEqual('PROTIEN');
   });
   // Read all Records
   it('Read all Food record', async () => {
-    const response = await request.get('/api/v2/food/');
+    let obj = { username: 'admin', password: 'password', role: 'admin' };
+    const response2 = await request.post('/signup').send(obj);
+    const response1 = await request.post('/signin')
+    .auth(obj.username, obj.password);
+    const userObject = response1.body;
+    const response = await request.get('/api/v2/food/').set('Authorization', `Bearer ${response1.body.token}`);
     expect(response.status).toEqual(200);
     expect(response.body[0].name).toEqual('Salmofish');
     expect(response.body[0].type).toEqual('PROTIEN');
   });
   // Delete a record
   it('Delete a Food record', async () => {
-    const response = await request.delete(`/api/v2/food/${id}`);
+    let obj = { username: 'admin', password: 'password', role: 'admin' };
+    const response2 = await request.post('/signup').send(obj);
+    const response1 = await request.post('/signin')
+    .auth(obj.username, obj.password);
+    const userObject = response1.body;
+    const response = await request.delete(`/api/v2/food/${id}`).set('Authorization', `Bearer ${response1.body.token}`);
     expect(response.status).toEqual(200);
   });
   ///////////////////////////////////////////////////////////////////////
+  // all the clothes tests here
   it('Create a Clothes record', async () => {
-    const response = await request.post('/api/v2/clothes/').send({
+    let obj = { username: 'admin', password: 'password', role: 'admin' };
+    const response2 = await request.post('/signup').send(obj);
+    const response1 = await request.post('/signin')
+    .auth(obj.username, obj.password);
+    const userObject = response1.body;
+    const response = await request.post('/api/v2/clothes/').set('Authorization', `Bearer ${response1.body.token}`).send({
         name: "highwaistJeans",
       color: "blue",
       size: "WomenClothing"
@@ -76,7 +101,12 @@ describe('Testing server', () => {
   });
   // Update a record 
   it('Update a Clothes record', async () => {
-    const response = await request.put(`/api/v2/clothes/${id}`).send({
+    let obj = { username: 'admin', password: 'password', role: 'admin' };
+    const response2 = await request.post('/signup').send(obj);
+    const response1 = await request.post('/signin')
+    .auth(obj.username, obj.password);
+    const userObject = response1.body;
+    const response = await request.put(`/api/v2/clothes/${id}`).set('Authorization', `Bearer ${response1.body.token}`).send({
       name: "highwaistJeans",
       color: "blue",
       size: "WomenClothing"
@@ -87,22 +117,51 @@ describe('Testing server', () => {
   });
   // Read a record
   it('Read a Clothes record', async () => {
-    const response = await request.get(`/api/v2/clothes/${id}`);
+    let obj = { username: 'admin', password: 'password', role: 'admin' };
+    const response2 = await request.post('/signup').send(obj);
+    const response1 = await request.post('/signin')
+    .auth(obj.username, obj.password);
+    const userObject = response1.body;
+    const response = await request.get(`/api/v2/clothes/${id}`).set('Authorization', `Bearer ${response1.body.token}`);
     expect(response.status).toEqual(200);
     expect(response.body.name).toEqual('highwaistJeans');
     expect(response.body.size).toEqual('WomenClothing');
   });
   // Read all Records
   it('Read all Clothes record', async () => {
-    const response = await request.get('/api/v2/clothes/');
+    let obj = { username: 'admin', password: 'password', role: 'admin' };
+    const response2 = await request.post('/signup').send(obj);
+    const response1 = await request.post('/signin')
+    .auth(obj.username, obj.password);
+    const userObject = response1.body;
+    const response = await request.get('/api/v2/clothes/').set('Authorization', `Bearer ${response1.body.token}`);
     expect(response.status).toEqual(200);
     expect(response.body[0].name).toEqual('highwaistJeans');
     expect(response.body[0].size).toEqual('WomenClothing');
   });
-  
   // Delete a record
   it('Delete a Clothes record', async () => {
-    const response = await request.delete(`/api/v2/clothes/${id}`);
+    let obj = { username: 'admin', password: 'password', role: 'admin' };
+    const response2 = await request.post('/signup').send(obj);
+    const response1 = await request.post('/signin')
+    .auth(obj.username, obj.password);
+    const userObject = response1.body;
+    const response = await request.delete(`/api/v2/clothes/${id}`).set('Authorization', `Bearer ${response1.body.token}`);
     expect(response.status).toEqual(200);
   });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
